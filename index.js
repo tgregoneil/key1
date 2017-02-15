f754d9db34904058b4e1c929872432cb9d06f407
T. Greg O'Neil <tgregoneil@gmail.com>
2017-02-15 10:03:27 -0600

    'reversed symbolic link structure between index.js and key1.js'

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-key1.js
+// key1.js
+
+// key1 extracts the single key from an object 
+// containing only one key/value pair
+// and returns the string value for the key
+// anything else passed to key1 returns null
+
+module.exports = (function () {
+
+//---------------------
+var key1 = (ob) => {
+
+    key = null;
+
+    var uniqueKeyExists = typeof ob !== 'undefined' &&
+                          ob !== null &&
+                          !Array.isArray(ob) &&
+                          typeof ob === 'object' &&
+                          Object.keys(ob).length === 1;
+    
+    if (uniqueKeyExists) {
+    
+        var keys = Object.keys(ob);
+        key = keys[0];
+    
+    } // end if (uniqueKeyExists)
+    
+    return key;
+    
+}; // end key1 
+
+return key1;
+
+}());
